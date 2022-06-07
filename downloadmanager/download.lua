b31ae64a343487882bce88c8417c694e6e37465a
DoggoProgrammer <85500383+DoggoProgrammer@users.noreply.github.com>
2022-06-07 17:32:38 +0200

    Update download.lua

--- downloadmanager/download.lua
+++ downloadmanager/download.lua
@@ -1,1 +1,5 @@
-
+-- BananenClient Download
+if not writefile or not rconsoleprint or not isfile or not readfile then game.Players.LocalPlayer:Kick("Your Executer does not support the Download From BananenClient.") end
+rconsoleprint("@@GREEN@@")
+rconsoleprint("\nBananenClient: Download...")
+if not isfile("bananenclient:downloadfiles.lua")
