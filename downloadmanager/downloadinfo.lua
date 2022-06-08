d28949f97bbde07722ae769dc7ffd521fd51b487
DoggoProgrammer <85500383+DoggoProgrammer@users.noreply.github.com>
2022-06-08 16:45:17 +0200

    Create downloadinfo.lua

---
+++ downloadmanager/downloadinfo.lua
@@ -0,0 +1,3 @@
+-- download information raw
+local information = "local c=Color3.fromRGB(0, 85, 0);local t=table;local s=string;local g=game;local p=game.Players.LocalPlayer;local ser={DSS='DataStoreService',HTTP='HttpService'}"
+return information
